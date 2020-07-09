3f4850cd50f9628cd277838e91d62fb67befb4cf
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-09 06:24:05 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,7 +1,11 @@
 def using_times
-	#your code here
+  using_times = 100
+ 
+  7.times do 
+    puts "Wingardium Leviosa"
+    using_times = using_times - 10
+    puts "Now there are only #{using_times} jewels left to hide!"
+  end
 end
-
-
 
 
